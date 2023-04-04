4665265f394318a5a89b94ddec2ec48052b5d78b
Sandrafongshurui <sandrafong90@hotmail.com>
2023-04-04 17:55:50 +0800

    feat: taix api

--- routes/data.js
+++ routes/data.js
@@ -12,10 +12,23 @@ const config = {
 router.get('/bus', async (req, res, next) => {
   try {
     const response = await Axios.get(
-      'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139',
+      'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=19029',
       config,
     )
-    const data = response.data.Services[0]
+    const data = response.data.Services[0].NextBus
+    console.log(data)
+    return res.status("200").json(data)
+  } catch (error) {
+    console.log('Axios error', error)
+  }
+})
+router.get('/taxi', async (req, res, next) => {
+  try {
+    const response = await Axios.get(
+      'http://datamall2.mytransport.sg/ltaodataservice/Taxi-Availability',
+      config,
+    )
+    const data = response.data.value[0]
     console.log(data)
     return res.status("200").json(data)
   } catch (error) {
