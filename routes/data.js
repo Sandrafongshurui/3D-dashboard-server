6f8e55f8cb18d656ca3f069e3201eae7d288184a
Sandrafongshurui <sandrafong90@hotmail.com>
2023-04-13 18:21:19 +0800

    add: train api

--- routes/data.js
+++ routes/data.js
@@ -17,7 +17,7 @@ router.get('/bus', async (req, res, next) => {
     )
     let data = response.data.Services
     data = data.map(item =>{
-     return [item.ServiceNo, item.NextBus]
+     return [item.ServiceNo, item.NextBus, item.NextBus2, item.NextBus3]
     })
     console.log(data)
     return res.status("200").json(data)
@@ -38,5 +38,19 @@ router.get('/taxi', async (req, res, next) => {
     console.log('Axios error', error)
   }
 })
+router.get('/platform-crowd', async (req, res, next) => {
+  try {
+    const response = await Axios.get(
+      'http://datamall2.mytransport.sg/ltaodataservice/PCDRealTime?TrainLine=EWL',
+      config,
+    )
+    const data = response.data.value
+    console.log(data)
+    return res.status("200").json(data)
+  } catch (error) {
+    console.log('Axios error', error)
+  }
+})
+
 
 module.exports = router
